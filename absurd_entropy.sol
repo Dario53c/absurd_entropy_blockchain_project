0b3ef1d68c24d7afdf07b343d371f70aaf88722a
Dario53c <dario.petric@stu.ibu.edu.ba>
2024-12-06 17:42:43 +0100

    blackjack declarations and shuffle logic

--- absurd_entropy.sol
+++ absurd_entropy.sol
@@ -19,4 +19,43 @@ contract absurd_entropy{
     constructor() {
         owner = msg.sender;
     }
+
+    function registerUser(string memory username) public {
+        Customers[msg.sender] = User(msg.sender, username, false);
+        isCustomer[msg.sender] = true;
+    }
+
+    function MakeVIP(address adresa) public onlyOwner{
+        Customers[adresa].VIP = true;
+    } 
+
+//blackjack logic----------------------------------------------------------------------
+
+    uint8[] public playerHand;
+    uint8[] public houseHand;
+    uint8 public playerScore;
+    uint8 public houseScore;
+    bool public gameStarted = false;
+    address public player;
+    uint8[] private deck;
+    uint256 private deckIndex;
+
+     // initialize deck and clear variables
+    function initializeDeck() internal {
+        delete deck;
+        delete playerHand;
+        delete houseHand;
+        playerScore = 0;
+        houseScore = 0;
+        deckIndex = 0;
+
+        for (uint8 i = 1; i <= 52; i++) {
+            deck.push(i);
+        }
+
+        for (uint256 i = 0; i < deck.length; i++) {
+            uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, i))) % deck.length;
+            (deck[i], deck[randomIndex]) = (deck[randomIndex], deck[i]);
+        }
+    }
 }
